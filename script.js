03cc7006d96d9261b0709aa445bf2e444d3bdfb1
Null Coder <princenarayan857@gmail.com>
2026-05-20 16:25:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const firstNumber = 5;
+const secondNumber = 10;
+
+// if remainder is 0 when divided by 2 number is even else it is odd
+if (firstNumber % 2 == 0) {
+    console.log("firstNumber is even and is",firstNumber);
+} else {
+    console.log("firstNumber is odd and is", firstNumber);
+}
+
+// is remainder is 1 when divided by 2 number is odd else it is even
+if (secondNumber % 2 != 0) {
+    console.log("secondNumber is odd and is",secondNumber);
+} else {
+    console.log("secondNumber is even and is", secondNumber);
+}
+
